2d5a37a609553284ef98f519ed6a4fba638a45e6
TolaMironcenko <mironcenkotola@gmail.com>
2024-10-11 23:49:35 +0700

    update threads in makefile

--- scripts/config.sh
+++ scripts/config.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 
 #------------ config ------------------
-version=0.0.1                               # script version
-root=$(pwd)                                 # root build directory
-disk="sdz"                                  # disk to write install image
-distro_name="Simple"                        # distro name
-distro_desc="Simple linux distro"           # distro desctiption
-distro_codename="zero_zero_one"             # distro codename
-hyperv_support="false"                      # hyperv support for kernel image
-out=$root/out                               # output directory
-build=$root/build                           # build directory
-sourcesfilename=sources.txt                 # sources file for downloading
-initramfsfile=$root/initramfs/initramfs.txt # file to created directories for initramfs
-initramfspath=$build/initramfs              # initramfs build directory
-rootfsfile=$root/rootfs/rootfs.txt          # file to created directories for rootfs
-rootfspath=$build/rootfs                    # rootfs build directory
-fulllogfile=$root/.log/fulllog.log          # file for full build logs
-makeflags=-j16                              # makeflags
+version=0.0.1                                  # script version
+root=$(pwd)                                    # root build directory
+disk="sdz"                                     # disk to write install image
+distro_name="Simple"                           # distro name
+distro_desc="Simple linux distro"              # distro desctiption
+distro_codename="zero_zero_one"                # distro codename
+hyperv_support="false"                         # hyperv support for kernel image
+out=$root/out                                  # output directory
+build=$root/build                              # build directory
+sourcesfilename=sources.txt                    # sources file for downloading
+initramfsfile=$root/initramfs/initramfs.txt    # file to created directories for initramfs
+initramfspath=$build/initramfs                 # initramfs build directory
+rootfsfile=$root/rootfs/rootfs.txt             # file to created directories for rootfs
+rootfspath=$build/rootfs                       # rootfs build directory
+fulllogfile=$root/.log/fulllog.log             # file for full build logs
+makeflags=-j$(grep -c processor /proc/cpuinfo) # makeflags
 #--------------------------------------
